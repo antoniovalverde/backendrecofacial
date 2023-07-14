3d261d7643d2aefeb2b4578b4952d4793b0b0cee
Antonio Valverde <antonio@valverdebalbuena.es>
2023-07-14 09:36:11 +0200

    clarifai by node

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,6 @@
 const handleApiCall = (req, res) => {
 
-	const raw = 
+	/*const raw = 
 			JSON.stringify({
 		    "user_app_id": {
 		      "user_id": "koky",
@@ -30,7 +30,64 @@ const handleApiCall = (req, res) => {
 	fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
 	.then(response => response.json())
 	.then(data => res.json(data))
-	.catch(err => res.status(400).json('unable to work with API'))
+	.catch(err => res.status(400).json('unable to work with API'))*/
+
+	// Your PAT (Personal Access Token) can be found in the portal under Authentification
+	const PAT = '3bb2abf8530f4c439bfd0458cf6e674c';
+	// Specify the correct user_id/app_id pairings
+	// Since you're making inferences outside your app's scope
+	const USER_ID = 'koky';
+	const APP_ID = 'test';
+	// Change these to whatever model and image URL you want to use
+	const MODEL_ID = 'face-detection';
+	const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+	const IMAGE_URL = req.body.input;
+
+	///////////////////////////////////////////////////////////////////////////////////
+	// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+	///////////////////////////////////////////////////////////////////////////////////
+
+	const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+	const stub = ClarifaiStub.grpc();
+
+	// This will be used by every Clarifai endpoint call
+	const metadata = new grpc.Metadata();
+	metadata.set("authorization", "Key " + PAT);
+
+	stub.PostModelOutputs(
+	    {
+	        user_app_id: {
+	            "user_id": USER_ID,
+	            "app_id": APP_ID
+	        },
+	        model_id: MODEL_ID,
+	        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+	        inputs: [
+	            { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+	        ]
+	    },
+	    metadata,
+	    (err, response) => {
+	        if (err) {
+	            throw new Error(err);
+	        }
+
+	        if (response.status.code !== 10000) {
+	            throw new Error("Post model outputs failed, status: " + response.status.description);
+	        }
+
+	        // Since we have one input, one output will exist here
+	        const output = response.outputs[0];
+
+	        /*console.log("Predicted concepts:");
+	        for (const concept of output.data.concepts) {
+	            console.log(concept.name + " " + concept.value);
+	        }*/
+	    }
+
+	);
+
 }
 
 const handleImage = (req, res, db) => {
